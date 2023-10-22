b3b59d7ce3b78d33ad9de385cc7380e594a6b1d0
Prathamesh Sable <prathameshks2003@gmail.com>
2023-10-22 20:22:50 +0530

    SEO optimization

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 export const backend_url = "https://thetechverse.pythonanywhere.com";
 // export const backend_url = "http://127.0.0.1:8000";
-
 export function setLoading(msg) {
     document.getElementById('spinner').classList.add('show');
     document.getElementById('loding_status').innerHTML = msg;
 }
-
 export function offLoading() {
     document.getElementById('spinner').classList.remove('show');
     document.getElementById('loding_status').innerHTML = "Loading";
 }
-
